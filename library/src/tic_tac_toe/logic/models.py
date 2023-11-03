e2dbd7a39e8e8c3d026933ed8b5ea2629a2c8c28
brian <riceb53@gmail.com>
2023-11-03 12:00:19 -0700

    add venv and verify grid

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -1,4 +1,6 @@
 import enum
+from dataclasses import dataclass
+import re
 
 class Mark(str, enum.Enum):
     CROSS = 'X'
@@ -6,6 +8,14 @@ class Mark(str, enum.Enum):
 
     @property
     def other(self) -> "Mark":
-        return Mark.CROSS if self is Mark.NAUGHT else Mark.CROSS
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
         #I think this should be return Mark.CROSS if self is Mark.NAUGHT else Mark.CROSS
 
+@dataclass(frozen=True)
+class Grid:
+    cells: str = " " * 9
+    
+    def __post_init__(self) => None:
+        if not re.match(r"^[\sXO]{9}$", self.cells):
+            raise ValueError("Must contain 9 cells of space/X/O")
+        
