ee73d180b0365de172d40d292d56aa2a42c5a3f0
brian <riceb53@gmail.com>
2023-11-05 12:43:07 -0800

    set up cli and play vs computer or two humans

--- frontends/console/renderers.py
+++ frontends/console/renderers.py
@@ -6,45 +6,45 @@ from tic_tac_toe.logic.models import GameState
 
 class ConsoleRenderer(Renderer):
     def render(self, game_state: GameState) -> None:
-        clear_screen()
-        print_solid(game_state.grid.cells)
+        clear_screen()        
         if game_state.winner:
             print_blinking(game_state.grid.cells, game_state.winning_cells)
-            print(f"{game_state.winner} wins \N{party_popper}")
+            print(f"{game_state.winner} wins")
+            # print(f"{game_state.winner} wins \N{party popper}")
         else:
             print_solid(game_state.grid.cells)
             if game_state.tie:
-                print("No one wins this time \N{neutral_face}")
+                print("No one wins this time \n{neutral_face}")
         
 
-    def clear_screen() -> None:
-        print("\033c", end="")
-    
-    def blink(text: str) -> str:
-        return f"\033[5m{text}\033[0m"
-    # should this be return f"\033[5m{text}\033]0m"
+def clear_screen() -> None:
+    print("\033c", end="")
 
-    def print_blinking(cells: Iterable[str], positions: Iterable[str]) -> None:
-        mutable_cells = list(cells)
-        for position in positions: 
-            mutable_cells[position] = blink(mutable_cells[position])
-        print_solid(mutable_cells)
+def blink(text: str) -> str:
+    return f"\033[5m{text}\033[0m"
+# should this be return f"\033[5m{text}\033]0m"
+
+def print_blinking(cells: Iterable[str], positions: Iterable[str]) -> None:
+    mutable_cells = list(cells)
+    for position in positions: 
+        mutable_cells[position] = blink(mutable_cells[position])
+    print_solid(mutable_cells)
 
 
-    def print_solid(cells: Iterable[str]) -> None:
-        print(
-            textwrap.dedent(
-                """\
-                A   B   C
-            ------------
-            1 ┆  {0} │ {1} │ {2}
-            ┆ ───┼───┼───
-            2 ┆  {3} │ {4} │ {5}
-            ┆ ───┼───┼───
-            3 ┆  {6} │ {7} │ {8}
-        """
-            ).format(*cells)
-        )
+def print_solid(cells: Iterable[str]) -> None:
+    print(
+        textwrap.dedent(
+            """\
+            A   B   C
+        ------------
+        1 ┆  {0} │ {1} │ {2}
+          ┆ ───┼───┼───
+        2 ┆  {3} │ {4} │ {5}
+          ┆ ───┼───┼───
+        3 ┆  {6} │ {7} │ {8}
+    """
+        ).format(*cells)
+    )
 
-    
-    
+
+
--- frontends/play.py
+++ frontends/play.py
@@ -1,10 +1,15 @@
+import pdb
+
 from tic_tac_toe.game.engine import TicTacToe
 from tic_tac_toe.game.players import RandomComputerPlayer
 from tic_tac_toe.logic.models import Mark
 
 from console.renderers import ConsoleRenderer
+from console.players import ConsolePlayer
 
-player1 = RandomComputerPlayer(Mark("X"))
+player1 = ConsolePlayer(Mark("X"))
 player2 = RandomComputerPlayer(Mark("O"))
 
-TicTacToe(player1, player2, ConsoleRenderer()).play
+# pdb.set_trace()
+
+TicTacToe(player1, player2, ConsoleRenderer()).play()
